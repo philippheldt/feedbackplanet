1fff96bdfd9eabfaf3e1e568b0c5e86d3fa9e8b7
Philipp Jenny <register@philippjenny.de>
2022-10-21 19:39:24 +0200

    adding automatic planetbuilder logic

--- gamestate-loader_fb.js
+++ gamestate-loader_fb.js
@@ -1,60 +1,86 @@
-window.addEventListener("load", () => {
-  //Insert Data
-  var planetContainer = document.querySelector(".pl-container");
-  planetContainer.innerHTML =
-    '<div class="sky"></div> <div class="tree-back"></div><div class="building"></div><div class="tree-front"></div><div class="planet"><img class="pl-asset" src="assets/img/planets/non.png" alt=""></div><div class="bush"></div>';
-  var obj;
-  var planet = document.querySelector(".planet");
-  var building = document.querySelector(".building");
-  var treeBack = document.querySelector(".tree-back");
-  var treeFront = document.querySelector(".tree-front");
-  var bushFront = document.querySelector(".bush");
+import { gamestate } from "./planetbuilder_fb.js";
+import { updateData } from "./planetbuilder_fb.js";
 
-  fetch("json/gamestate.json")
-    .then((res) => res.json())
-    .then((data) => (obj = data))
-    .then(() => {
-      obj.points;
-      console.log(obj.planet);
+//Insert Data
+var planetContainer = document.querySelector(".pl-container");
+planetContainer.innerHTML =
+  '<div class="sky"></div> <div class="tree-back"></div><div class="building"></div><div class="tree-front"></div><div class="planet"><img class="pl-asset" src="assets/img/planets/non.png" alt=""></div><div class="bush"></div>';
 
-      //setup
-      //create planet
-      planet.innerHTML = `<img class="pl-asset" src="assets/img/planets/${obj.planet}.png" alt="">`;
+var planet = document.querySelector(".planet");
+var building = document.querySelector(".building");
+var treeBack = document.querySelector(".tree-back");
+var treeFront = document.querySelector(".tree-front");
+var bushFront = document.querySelector(".bush");
 
-      //create all objects
+function createPlanet() {
+  //setup
+  //create planet
+  planet.innerHTML = `<img class="pl-asset" src="assets/img/planets/${gamestate.planet}.png" alt="">`;
 
-      for (let i = 0; i < obj.buildings.length; i++) {
-        let buildingSetup = obj.buildings[i].split(".");
-        //adding building to scene
-        if (buildingSetup[0] != "non0") {
-          let builingImage = document.createElement("img");
-          builingImage.setAttribute("src", `assets/img/buildings/${buildingSetup[0]}.png`);
-          builingImage.classList.add(`pl-asset`);
-          builingImage.classList.add(`building-${i + 1}`);
-          building.appendChild(builingImage);
-        }
-        //adding trees to scene
-        for (let tree = 1; tree <= 4; tree++) {
-          if (buildingSetup[tree] != "no0" && tree <= 2) {
-            let treeImage = document.createElement("img");
-            treeImage.setAttribute("src", `assets/img/trees_back/${buildingSetup[tree]}.png`);
-            treeImage.classList.add(`pl-asset`);
-            treeImage.classList.add(`tree-back-${i + i + tree}`);
-            treeBack.appendChild(treeImage);
-          } else if (buildingSetup[tree] != "no0" && tree === 3) {
-            let treeImage = document.createElement("img");
-            treeImage.setAttribute("src", `assets/img/trees_front/${buildingSetup[tree]}.png`);
-            treeImage.classList.add(`pl-asset`);
-            treeImage.classList.add(`tree-front-${i + tree - 2}`);
-            treeFront.appendChild(treeImage);
-          } else if (buildingSetup[tree] != "no0" && tree === 4) {
-            let treeImage = document.createElement("img");
-            treeImage.setAttribute("src", `assets/img/trees_front/${buildingSetup[tree]}.png`);
-            treeImage.classList.add(`pl-asset`);
-            treeImage.classList.add(`bush-${i + tree - 3}`);
-            treeFront.appendChild(treeImage);
-          }
-        }
+  console.log(gamestate);
+  //create all objects
+
+  for (let i = 0; i < gamestate.buildings.length; i++) {
+    let buildingSetup = gamestate.buildings[i].split(".");
+    //adding building to scene
+    if (buildingSetup[0] != "non0") {
+      let builingImage = document.createElement("img");
+      builingImage.setAttribute("src", `assets/img/buildings/${buildingSetup[0]}.png`);
+      builingImage.classList.add(`pl-asset`);
+      builingImage.classList.add(`building-${i + 1}`);
+      building.appendChild(builingImage);
+    }
+    //adding trees to scene
+    for (let tree = 1; tree <= 4; tree++) {
+      if (buildingSetup[tree] != "no0" && tree <= 2) {
+        let treeImage = document.createElement("img");
+        treeImage.setAttribute("src", `assets/img/trees_back/${buildingSetup[tree]}.png`);
+        treeImage.classList.add(`pl-asset`);
+        treeImage.classList.add(`tree-back-${i + i + tree}`);
+        treeBack.appendChild(treeImage);
+      } else if (buildingSetup[tree] != "no0" && tree === 3) {
+        let treeImage = document.createElement("img");
+        treeImage.setAttribute("src", `assets/img/trees_front/${buildingSetup[tree]}.png`);
+        treeImage.classList.add(`pl-asset`);
+        treeImage.classList.add(`tree-front-${i + tree - 2}`);
+        treeFront.appendChild(treeImage);
+      } else if (buildingSetup[tree] != "no0" && tree === 4) {
+        let treeImage = document.createElement("img");
+        treeImage.setAttribute("src", `assets/img/trees_front/${buildingSetup[tree]}.png`);
+        treeImage.classList.add(`pl-asset`);
+        treeImage.classList.add(`bush-${i + tree - 3}`);
+        treeFront.appendChild(treeImage);
       }
-    });
+    }
+  }
+}
+
+document.querySelector("#buibtn").addEventListener("click", createPlanet);
+
+//Update Data from Textfield input
+
+// Get the input box
+let input = document.getElementById("feedback-input");
+
+// Init a timeout variable to be used below
+let timeout = null;
+let animationStarted = false;
+
+// Listen for keystroke events
+input.addEventListener("keyup", function (e) {
+  // Clear the timeout if it has already been set.
+  // This will prevent the previous task from executing
+  // if it has been less than <MILLISECONDS>
+  !animationStarted ? planetContainer.classList.add("typing") : null;
+  animationStarted = true;
+  clearTimeout(timeout);
+
+  // Make a new timeout set to go off in 1000ms (1 second)
+  timeout = setTimeout(function () {
+    planetContainer.classList.remove("typing");
+    animationStarted = false;
+    gamestate.points = input.value.length;
+    console.log(gamestate.points);
+    updateData();
+  }, 250);
 });
--- planetbuilder_fb.js
+++ planetbuilder_fb.js
@@ -71,16 +71,6 @@ const gamestate = {
 function insertData() {
   set(ref(db, "feedbackplanet/" + username.value), {
     username: username.value,
-    building1: building1.value,
-    building2: building2.value,
-    building3: building3.value,
-    building4: building4.value,
-    building5: building5.value,
-    building6: building6.value,
-    building7: building7.value,
-    building8: building8.value,
-    points: points.value,
-    planet: planet.value,
   })
     .then(() => {
       console.log("Data saved successfully");
@@ -99,21 +89,19 @@ function getData() {
   get(child(dbref, "feedbackplanet/" + username.value))
     .then((snapshot) => {
       if (snapshot.exists()) {
-        console.log(snapshot.val());
-        building1.value = snapshot.val().building1;
-        building2.value = snapshot.val().building2;
-        building3.value = snapshot.val().building3;
-        building4.value = snapshot.val().building4;
-        building5.value = snapshot.val().building5;
-        building6.value = snapshot.val().building6;
-        building7.value = snapshot.val().building7;
-        building8.value = snapshot.val().building8;
-        points.value = snapshot.val().points;
-        planet.value = snapshot.val().planet;
+        //console.log(snapshot.val());
+        gamestate.username = snapshot.val().username;
+        for (let i = 0; i < 8; i++) {
+          gamestate.buildings[i] = snapshot.val()["building" + (i + 1)];
+        }
+        gamestate.points = snapshot.val().points;
+        gamestate.planet = snapshot.val().planet;
+        console.log(gamestate);
 
+        //Login
         homescreen.classList.remove("hidden");
         login.classList.add("hidden");
-        welcomeMessage.innerHTML = "Welcome " + snapshot.val().username;
+        welcomeMessage.innerHTML = "Welcome " + gamestate.username;
       } else {
         console.log("No data available");
       }
@@ -128,16 +116,16 @@ selbtn.addEventListener("click", getData);
 //Update data to firebase
 function updateData() {
   update(ref(db, "feedbackplanet/" + username.value), {
-    building1: building1.value,
-    building2: building2.value,
-    building3: building3.value,
-    building4: building4.value,
-    building5: building5.value,
-    building6: building6.value,
-    building7: building7.value,
-    building8: building8.value,
-    points: points.value,
-    planet: planet.value,
+    building1: gamestate.buildings[0],
+    building2: gamestate.buildings[1],
+    building3: gamestate.buildings[2],
+    building4: gamestate.buildings[3],
+    building5: gamestate.buildings[4],
+    building6: gamestate.buildings[5],
+    building7: gamestate.buildings[6],
+    building8: gamestate.buildings[7],
+    points: gamestate.points,
+    planet: gamestate.planet,
   })
     .then(() => {
       console.log("Data saved successfully");
@@ -162,3 +150,171 @@ function removeData() {
 }
 //Button Event
 delbtn.addEventListener("click", removeData);
+
+///////////////////////////////////////////////////////////////////////////////////
+
+var planetContainer = document.querySelector(".pl-container");
+planetContainer.innerHTML =
+  '<div class="sky"></div> <div class="tree-back"></div><div class="building"></div><div class="tree-front"></div><div class="planet"><img class="pl-asset" src="assets/img/planets/non.png" alt=""></div><div class="bush"></div>';
+
+var planet = document.querySelector(".planet");
+var building = document.querySelector(".building");
+var treeBack = document.querySelector(".tree-back");
+var treeFront = document.querySelector(".tree-front");
+var bushFront = document.querySelector(".bush");
+
+function createPlanet() {
+  //setup
+  //create planet
+  planet.innerHTML = `<img class="pl-asset" src="assets/img/planets/${gamestate.planet}.png" alt="">`;
+
+  console.log(gamestate);
+  //create all objects
+  building.innerHTML = "";
+  treeBack.innerHTML = "";
+  treeFront.innerHTML = "";
+  for (let i = 0; i < gamestate.buildings.length; i++) {
+    let buildingSetup = gamestate.buildings[i].split(".");
+    //adding building to scene
+    if (buildingSetup[0] != "non0") {
+      let builingImage = document.createElement("img");
+      builingImage.setAttribute("src", `assets/img/buildings/${buildingSetup[0]}.png`);
+      builingImage.classList.add(`pl-asset`);
+      builingImage.classList.add(`building-${i + 1}`);
+      building.appendChild(builingImage);
+    }
+    //adding trees to scene
+    for (let tree = 1; tree <= 4; tree++) {
+      if (buildingSetup[tree] != "no0" && tree <= 2) {
+        let treeImage = document.createElement("img");
+        treeImage.setAttribute("src", `assets/img/trees_back/${buildingSetup[tree]}.png`);
+        treeImage.classList.add(`pl-asset`);
+        treeImage.classList.add(`tree-back-${i + i + tree}`);
+        treeBack.appendChild(treeImage);
+      } else if (buildingSetup[tree] != "no0" && tree === 3) {
+        let treeImage = document.createElement("img");
+        treeImage.setAttribute("src", `assets/img/trees_front/${buildingSetup[tree]}.png`);
+        treeImage.classList.add(`pl-asset`);
+        treeImage.classList.add(`tree-front-${i + tree - 2}`);
+        treeFront.appendChild(treeImage);
+      } else if (buildingSetup[tree] != "no0" && tree === 4) {
+        let treeImage = document.createElement("img");
+        treeImage.setAttribute("src", `assets/img/trees_front/${buildingSetup[tree]}.png`);
+        treeImage.classList.add(`pl-asset`);
+        treeImage.classList.add(`bush-${i + tree - 3}`);
+        treeFront.appendChild(treeImage);
+      }
+    }
+  }
+}
+
+document.querySelector("#buibtn").addEventListener("click", createPlanet);
+
+//Update Data from Textfield input
+
+// Get the input box
+let input = document.getElementById("feedback-input");
+let tempPoints = 0;
+// Init a timeout variable to be used below
+let timeout = null;
+let animationStarted = false;
+
+// Listen for keystroke events
+input.addEventListener("keyup", function (e) {
+  // Clear the timeout if it has already been set.
+  // This will prevent the previous task from executing
+  // if it has been less than <MILLISECONDS>
+  !animationStarted ? planetContainer.classList.add("typing") : null;
+  animationStarted = true;
+  clearTimeout(timeout);
+
+  // Make a new timeout set to go off in 1000ms (1 second)
+  timeout = setTimeout(function () {
+    planetContainer.classList.remove("typing");
+    animationStarted = false;
+    gamestate.points += input.value.length - tempPoints;
+    tempPoints += input.value.length - tempPoints;
+    console.log(gamestate.points);
+    updateData();
+    updateBuildings();
+  }, 250);
+});
+
+function updateBuildings() {
+  const currentBuilding = gamestate.buildings[0].split(".")[0];
+  const buildingStage = Math.floor(gamestate.points / 100 + 1);
+  const pointsFromLastBuilding = gamestate.points - (buildingStage - 1) * 100;
+  console.log("From last Stage: " + pointsFromLastBuilding);
+  let newBuilding = "iba" + Math.floor(gamestate.points / 100 + 1);
+  if (currentBuilding != newBuilding && gamestate.points <= 500) {
+    gamestate.buildings[0] = newBuilding + ".no0.no0.no0.no0";
+    updateData();
+    createPlanet();
+  }
+
+  if (pointsFromLastBuilding < 25 && gamestate.buildings[0].split(".")[1] === "no0") {
+    gamestate.buildings[0] =
+      gamestate.buildings[0].split(".")[0] +
+      "." +
+      "pi1" +
+      "." +
+      gamestate.buildings[0].split(".")[2] +
+      "." +
+      gamestate.buildings[0].split(".")[3] +
+      "." +
+      gamestate.buildings[0].split(".")[4];
+    updateData();
+    createPlanet();
+  } else if (
+    pointsFromLastBuilding >= 25 &&
+    pointsFromLastBuilding < 50 &&
+    gamestate.buildings[0].split(".")[2] === "no0"
+  ) {
+    gamestate.buildings[0] =
+      gamestate.buildings[0].split(".")[0] +
+      "." +
+      gamestate.buildings[0].split(".")[1] +
+      "." +
+      "pi1" +
+      "." +
+      gamestate.buildings[0].split(".")[3] +
+      "." +
+      gamestate.buildings[0].split(".")[4];
+    updateData();
+    createPlanet();
+  } else if (
+    pointsFromLastBuilding >= 50 &&
+    pointsFromLastBuilding < 75 &&
+    gamestate.buildings[0].split(".")[3] === "no0"
+  ) {
+    gamestate.buildings[0] =
+      gamestate.buildings[0].split(".")[0] +
+      "." +
+      gamestate.buildings[0].split(".")[1] +
+      "." +
+      gamestate.buildings[0].split(".")[2] +
+      "." +
+      "pi1" +
+      "." +
+      gamestate.buildings[0].split(".")[4];
+    updateData();
+    createPlanet();
+  } else if (
+    pointsFromLastBuilding >= 75 &&
+    pointsFromLastBuilding < 100 &&
+    gamestate.buildings[0].split(".")[4] === "no0"
+  ) {
+    gamestate.buildings[0] =
+      gamestate.buildings[0].split(".")[0] +
+      "." +
+      gamestate.buildings[0].split(".")[1] +
+      "." +
+      gamestate.buildings[0].split(".")[2] +
+      "." +
+      gamestate.buildings[0].split(".")[3] +
+      "." +
+      "pi1";
+    updateData();
+    createPlanet();
+  }
+}
