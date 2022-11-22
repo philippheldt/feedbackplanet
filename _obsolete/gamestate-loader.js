49b1f86cdf33fca63c7de8f7b81798a3259261e8
Philipp Jenny <register@philippjenny.de>
2022-11-22 11:38:02 +0100

    updating references

--- _obsolete/gamestate-loader.js
+++ _obsolete/gamestate-loader.js
@@ -2,7 +2,7 @@ window.addEventListener("load", () => {
   //Insert Data
   var planetContainer = document.querySelector(".pl-container");
   planetContainer.innerHTML =
-    '<div class="sky"></div> <div class="tree-back"></div><div class="building"></div><div class="tree-front"></div><div class="planet"><img class="pl-asset" src="assets/img/planets/non.png" alt=""></div><div class="bush"></div>';
+    '<div class="sky"></div> <div class="tree-back"></div><div class="building"></div><div class="tree-front"></div><div class="planet"><img class="pl-asset" src="../assets/img/planets/non.png" alt=""></div><div class="bush"></div>';
 
   var obj;
   var planet = document.querySelector(".planet");
@@ -20,7 +20,7 @@ window.addEventListener("load", () => {
 
       //setup
       //create planet
-      planet.innerHTML = `<img class="pl-asset" src="assets/img/planets/${obj.planet}.png" alt="">`;
+      planet.innerHTML = `<img class="pl-asset" src="../assets/img/planets/${obj.planet}.png" alt="">`;
 
       //create all objects
 
@@ -29,7 +29,7 @@ window.addEventListener("load", () => {
         //adding building to scene
         if (buildingSetup[0] != "non0") {
           let builingImage = document.createElement("img");
-          builingImage.setAttribute("src", `assets/img/buildings/${buildingSetup[0]}.png`);
+          builingImage.setAttribute("src", `../assets/img/buildings/${buildingSetup[0]}.png`);
           builingImage.classList.add(`pl-asset`);
           builingImage.classList.add(`building-${i + 1}`);
           building.appendChild(builingImage);
@@ -38,19 +38,19 @@ window.addEventListener("load", () => {
         for (let tree = 1; tree <= 4; tree++) {
           if (buildingSetup[tree] != "no0" && tree <= 2) {
             let treeImage = document.createElement("img");
-            treeImage.setAttribute("src", `assets/img/trees_back/${buildingSetup[tree]}.png`);
+            treeImage.setAttribute("src", `../assets/img/trees_back/${buildingSetup[tree]}.png`);
             treeImage.classList.add(`pl-asset`);
             treeImage.classList.add(`tree-back-${i + i + tree}`);
             treeBack.appendChild(treeImage);
           } else if (buildingSetup[tree] != "no0" && tree === 3) {
             let treeImage = document.createElement("img");
-            treeImage.setAttribute("src", `assets/img/trees_front/${buildingSetup[tree]}.png`);
+            treeImage.setAttribute("src", `../assets/img/trees_front/${buildingSetup[tree]}.png`);
             treeImage.classList.add(`pl-asset`);
             treeImage.classList.add(`tree-front-${i + tree - 2}`);
             treeFront.appendChild(treeImage);
           } else if (buildingSetup[tree] != "no0" && tree === 4) {
             let treeImage = document.createElement("img");
-            treeImage.setAttribute("src", `assets/img/trees_front/${buildingSetup[tree]}.png`);
+            treeImage.setAttribute("src", `../assets/img/trees_front/${buildingSetup[tree]}.png`);
             treeImage.classList.add(`pl-asset`);
             treeImage.classList.add(`bush-${i + tree - 3}`);
             treeFront.appendChild(treeImage);
