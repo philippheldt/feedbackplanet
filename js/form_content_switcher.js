bb04263f5d39589c01e4d0f6dd369ba753b9aba4
Philipp Jenny <register@philippjenny.de>
2022-12-05 17:00:15 +0100

    improving final consent question

--- js/form_content_switcher.js
+++ js/form_content_switcher.js
@@ -5,7 +5,7 @@ const submitButton = document.querySelector("#submit");
 const statusAmount = document.querySelector(".status-amount");
 
 let containerPosition = 1;
-const containerPositionMax = 9;
+const containerPositionMax = 10;
 
 statusAmount.style.width = (100 / containerPositionMax) * containerPosition + "%";
 
