1967a6e599d25f217f2a16334e1245b0f9ba84f3
Philipp Jenny <register@philippjenny.de>
2023-01-06 11:29:46 +0100

    mobile fix in main

--- js/planetbuilder_fb.js
+++ js/planetbuilder_fb.js
@@ -160,7 +160,13 @@ import { getBrowser } from "./checkBrowser.js";
 
 var planetContainer = document.querySelector(".pl-container");
 
-if (getBrowser() == "Safari") {
+function isMobileDevice() {
+  return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+    navigator.userAgent || navigator.vendor || window.opera
+  );
+}
+
+if (getBrowser() == "Safari" || isMobileDevice()) {
   planetContainer.innerHTML =
     '<div class="sky"> <img id="background-image-sky" src="" ></img></div> <div class="tree-back"></div><div class="building"></div><div class="tree-front"></div><div class="planet"><img class="pl-asset" src="assets/planet_assets/planets/nono.png" id="planet-background"></div><div class="bush"></div>';
 } else {
